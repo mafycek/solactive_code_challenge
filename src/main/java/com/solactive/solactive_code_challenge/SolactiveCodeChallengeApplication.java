94fae0f9cb30214bc3074c321457dbada970e517
mafycek <h.lavicka@email.cz>
2021-04-28 21:49:05 +0200

    Initial commit

---
+++ src/main/java/com/solactive/solactive_code_challenge/SolactiveCodeChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.solactive.solactive_code_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolactiveCodeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolactiveCodeChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solactive/solactive_code_challenge/SolactiveCodeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solactive.solactive_code_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolactiveCodeChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
